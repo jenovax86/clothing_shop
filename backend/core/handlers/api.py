1e9d3c313f1016bf9134e7dea1fc6d92c1d4c2e7
jenovax86 <shruzvn@gmail.com>
2026-07-28 03:08:48 +0330

    Add exception handler

---
+++ backend/core/handlers/api.py
@@ -0,0 +1,27 @@
+from typing import Any
+from rest_framework.views import exception_handler as drf_exception_handler
+from rest_framework.response import Response
+from core.exceptions import AbstractException
+
+
+def api_exception_handler(exception: Exception, context: dict[str, Any]) -> Response | None:
+    response = drf_exception_handler(exception, context)
+
+    if response is not None:
+        return response
+
+    if isinstance(exception, AbstractException):
+        return Response(
+            {
+                "success": False,
+                "message": exception.error_code,
+            },
+            status=exception.status_code,
+        )
+    return Response(
+        {
+            "success": False,
+            "message": "Internal server error",
+        },
+        status=500,
+    )
