0cf877e3e13028b98ed07ec84ec503d5705e47d8
jenovax86 <shruzvn@gmail.com>
2026-07-30 06:55:58 +0330

    Remove unnecessary imports

--- backend/apps/test_app/views.py
+++ backend/apps/test_app/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
+from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from core.exceptions import NotFoundException, ConflictException
 
 
 class TestView(APIView):
     def get(self, request):
-        raise ConflictException()
+        return Response({"test": "test"})
