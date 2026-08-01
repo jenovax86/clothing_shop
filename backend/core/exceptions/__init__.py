d6e6f38d9dfbe21ef4be007aae93b853c1ca9395
jenovax86 <shruzvn@gmail.com>
2026-08-01 10:24:54 +0330

    Add customs exceptions to init file

--- backend/core/exceptions/__init__.py
+++ backend/core/exceptions/__init__.py
@@ -1,3 +1,4 @@
 from .base import AbstractException, NotFoundException, ConflictException
+from .user import UserDidNotFound, PasswordDidNotMatch
 
-__all__ = ["AbstractException", "NotFoundException", "ConflictException"]
+__all__ = ["AbstractException", "NotFoundException", "ConflictException", "UserDidNotFound", "PasswordDidNotMatch"]
