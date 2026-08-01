9442b88c3efee4442632ba1d6e430debd4d78438
jenovax86 <shruzvn@gmail.com>
2026-08-01 10:25:27 +0330

    Add logger for logging exception

--- backend/core/handlers/api.py
+++ backend/core/handlers/api.py
@@ -1,10 +1,15 @@
+import logging
 from typing import Any
 from rest_framework.views import exception_handler as drf_exception_handler
 from rest_framework.response import Response
+
 from core.exceptions import AbstractException
+
+logger = logging.getLogger(__name__)
 
 
 def api_exception_handler(exception: Exception, context: dict[str, Any]) -> Response:
+    logger.exception(exception)
     response = drf_exception_handler(exception, context)
 
     if response is not None:
