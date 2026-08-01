8bd4e42314df555beb3ee10413013954e6190bee
jenovax86 <shruzvn@gmail.com>
2026-08-01 10:34:06 +0330

    Add auth urls in project

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/test_app/", include("apps.test_app.urls")),
+    path("api/v1/", include("apps.authentication.urls")),
 ]
