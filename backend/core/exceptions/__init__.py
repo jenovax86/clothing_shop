99934da46868f691af04afa9ac29768b67b12eec
jenovax86 <shruzvn@gmail.com>
2026-08-07 04:06:03 +0330

    Add token service error exception

--- backend/core/exceptions/__init__.py
+++ backend/core/exceptions/__init__.py
@@ -1,4 +1,5 @@
 from .base import AbstractException, NotFoundException, ConflictException
 from .user import UserDidNotFound, PasswordDidNotMatch
+from .authentication import TokenNotFound, TokenInvalid, TokenExpired
 
-__all__ = ["AbstractException", "NotFoundException", "ConflictException", "UserDidNotFound", "PasswordDidNotMatch"]
+__all__ = ["AbstractException", "NotFoundException", "ConflictException", "UserDidNotFound", "PasswordDidNotMatch", "TokenNotFound", "TokenInvalid", "TokenExpired"]
--- backend/core/exceptions/authentication.py
+++ backend/core/exceptions/authentication.py
@@ -7,3 +7,17 @@ class AuthenticationFailed(AbstractException):
     status_code = HTTPStatus.CONFLICT
     error_code = "Authentication failed"
 
+
+class TokenExpired(AbstractException):
+    status_code = HTTPStatus.CONFLICT
+    error_code = "Token expired"
+
+
+class TokenInvalid(AbstractException):
+    status_code = HTTPStatus.CONFLICT
+    error_code = "Token invalid"
+
+
+class TokenNotFound(AbstractException):
+    status_code = HTTPStatus.CONFLICT
+    error_code = "Token not found"
