246c1041faee9bce864c10a8f3b8a648f71bd672
jenovax86 <shruzvn@gmail.com>
2026-08-07 04:06:56 +0330

    Remove duplicate import

--- backend/apps/authentication/services.py
+++ backend/apps/authentication/services.py
@@ -6,7 +6,7 @@ from argon2.exceptions import VerifyMismatchError
 from logging import getLogger
 
 from apps.users.models import User
-from core.exceptions import UserDidNotFound, PasswordDidNotMatch
+from core.exceptions import UserDidNotFound, PasswordDidNotMatch, TokenNotFound, TokenInvalid, TokenExpired
 
 logger = getLogger(__name__)
 
@@ -14,7 +14,7 @@ logger = getLogger(__name__)
 class TokenService:
 
     @staticmethod
-    def generate_access_token(user_id: int):
+    def generate_access_token(user_id: int) -> str:
         logger.info(f"Generating token for user {user_id}")
         payload = {
             'user_id': user_id,
@@ -23,6 +23,26 @@ class TokenService:
         }
         logger.info(f"payload: {payload}")
         return jwt.encode(payload, os.getenv('JWT_SECRET'), algorithm='HS256')
+
+    @staticmethod
+    def verify_token(token: str) -> dict:
+        print(jwt.exceptions.ExpiredSignatureError)
+        print(jwt.exceptions.InvalidSignatureError)
+        print(jwt.exceptions.InvalidTokenError)
+        logger.info(f"Decode token {token}")
+        if not token:
+            logger.warning("Token not found")
+            raise TokenNotFound("Token not found")
+        try:
+            valid_token: str = token.split(" ")[1]
+            decoded_token: dict = jwt.decode(valid_token, os.getenv('JWT_SECRET'), algorithms=['HS256'])
+            return decoded_token
+        except jwt.InvalidTokenError:
+            logger.warning("Invalid token")
+            raise TokenInvalid("Invalid token")
+        except jwt.ExpiredSignatureError:
+            logger.warning("Expired token")
+            raise TokenExpired("Expired token")
 
 
 class AuthenticationService:
