5f9e9b4cba515517c2fce08eda6143a2a578e97d
jenovax86 <shruzvn@gmail.com>
2026-08-07 04:04:30 +0330

    Add role field in table

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 
+class Role(models.TextChoices):
+    ADMIN = ("admin", "Admin")
+    USER = ("user", "User")
+
+
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20, unique=True, null=False, blank=False)
     password = models.CharField(max_length=100, null=False, blank=False)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
