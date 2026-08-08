633872e85f5a780ec723da28400995c1346d680a
jenovax86 <shruzvn@gmail.com>
2026-08-08 05:47:37 +0330

    change url of users

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/test_app/", include("apps.test_app.urls")),
-    path("api/v1/", include("apps.authentication.urls")),
+    path("api/v1/auth/", include("apps.authentication.urls")),
+    path("api/v1/", include("apps.users.urls")),
 ]
