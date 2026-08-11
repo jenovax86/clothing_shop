78adba51f5ace218ec4cb428b64e0be219c61654
jenovax86 <shruzvn@gmail.com>
2026-08-11 16:54:40 +0330

    Add categories api urls

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import CreateCategory
+from .views import CreateCategory, ListCategories, GetCategory, UpdateCategory, DeleteCategory
 
 urlpatterns = [
-    path('categories/create', CreateCategory.as_view(), name='create-category'),
+    path('categories/create/', CreateCategory.as_view(), name='create-category'),
+    path('categories/all/', ListCategories.as_view(), name='list-categories'),
+    path('categories/get/<int:category_id>/', GetCategory.as_view(), name='get-category'),
+    path('categories/update/<int:category_id>/', UpdateCategory.as_view(), name='update_category'),
+    path('categories/delete/<int:category_id>/', DeleteCategory.as_view(), name='update_category'),
 ]
