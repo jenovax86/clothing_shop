ea61debf4ac3253062936e90964a57f424c1d91b
jenovax86 <shruzvn@gmail.com>
2026-08-11 16:17:32 +0330

    Add category exceptions

--- backend/core/exceptions/__init__.py
+++ backend/core/exceptions/__init__.py
@@ -2,8 +2,10 @@ from .base import AbstractException, NotFoundException, ConflictException
 from .user import UserDidNotFound, PasswordDidNotMatch, UsernameIsRequired, UserAlreadyExists, PasswordIsRequired, \
     PasswordIsIdentical, AddressCantBeEmpty, AddressAlreadyExists, AddressDoesNotExist
 from .authentication import TokenNotFound, TokenInvalid, TokenExpired
+from .category import CategoryAlreadyExists, CategoryDoesNotExist
 
 __all__ = ["AbstractException", "NotFoundException", "ConflictException", "UserDidNotFound", "PasswordDidNotMatch",
            "TokenNotFound", "TokenInvalid", "TokenExpired", "UsernameIsRequired", "UserAlreadyExists",
            "PasswordIsIdentical",
-           "PasswordIsRequired", "AddressCantBeEmpty", "AddressAlreadyExists", "AddressDoesNotExist"]
+           "PasswordIsRequired", "AddressCantBeEmpty", "AddressAlreadyExists", "AddressDoesNotExist",
+           "CategoryDoesNotExist", "CategoryAlreadyExists", ]
