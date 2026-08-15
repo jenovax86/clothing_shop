ee02d6f56f2acdaa087c2288043281d6f758d542
jenovax86 <shruzvn@gmail.com>
2026-08-15 17:54:44 +0330

    Refactor use request.user instead of decoding and get user

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -15,13 +15,10 @@ logger = logging.getLogger(__name__)
 class ChangeUsername(APIView):
     def patch(self, request):
         logger.info(f"User requested")
-        decoded_token = TokenService.decode_token(request.headers.get("Authorization").split(" ")[1])
-        user = UserService.find_user_by_id(decoded_token.get("user_id")).username
         serializer = ChangeUsernameSerializer(data=request.data)
-
         if serializer.is_valid():
-            UserService.change_username(user, serializer.validated_data["username"])
-            logger.info(f"User {user} changed successfully")
+            UserService.change_username(request.user.username, serializer.validated_data["username"])
+            logger.info(f"User {request.user} changed successfully")
             return Response({
                 "success": True,
                 "message": "User changed successfully",
@@ -32,17 +29,15 @@ class ChangeUsername(APIView):
 class ChangePassword(APIView):
     def patch(self, request):
         logger.info(f"User password requested")
-        decoded_token = TokenService.decode_token(request.headers.get("Authorization").split(" ")[1])
-        user = UserService.find_user_by_id(decoded_token.get("user_id"))
         serializer = ChangePasswordSerializer(data=request.data)
 
         if serializer.is_valid():
             old_password = serializer.validated_data["old_password"]
             new_password = serializer.validated_data["new_password"]
             if old_password != new_password:
-                UserService.change_password(user, serializer.validated_data["old_password"],
+                UserService.change_password(request.user, serializer.validated_data["old_password"],
                                             serializer.validated_data["new_password"])
-                logger.info(f"User {user} changed successfully")
+                logger.info(f"User {request.user} changed successfully")
                 return Response({
                     "success": True,
                     "message": "Password changed successfully",
@@ -55,15 +50,13 @@ class ChangePassword(APIView):
 class CreateAddress(APIView):
     def post(self, request):
         logger.info(f"Address requested")
-        decoded_token = TokenService.decode_token(request.headers.get("Authorization").split(" ")[1])
-        user = UserService.find_user_by_id(decoded_token.get("user_id"))
         serializer = AddressSerializer(data=request.data)
         if serializer.is_valid():
-            UserService.add_user_address(user=user, country=serializer.validated_data["country"],
+            UserService.add_user_address(user=request.user, country=serializer.validated_data["country"],
                                          zip_code=serializer.validated_data["zip_code"],
                                          city=serializer.validated_data["city"],
                                          province=serializer.validated_data["province"])
-            logger.info(f"Create {user.username} address")
+            logger.info(f"Create {request.user.username} address")
             return Response({
                 "success": True,
                 "message": "Address created successfully",
@@ -74,12 +67,10 @@ class CreateAddress(APIView):
 class ChangeAddress(APIView):
     def patch(self, request, address_id):
         logger.info(f"Address requested for changing")
-        decoded_token = TokenService.decode_token(request.headers.get("Authorization").split(" ")[1])
-        user = UserService.find_user_by_id(decoded_token.get("user_id"))
         serializer = AddressSerializer(data=request.data, partial=True)
 
         if serializer.is_valid():
-            UserService.change_user_address(user, address_id, **serializer.validated_data)
+            UserService.change_user_address(request.user, address_id, **serializer.validated_data)
             logger.info("Changed Address successfully")
             return Response({
                 "success": True,
