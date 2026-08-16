505e867f4edfc7ce89a009ba9fd620935e6717a4
jenovax86 <shruzvn@gmail.com>
2026-08-16 18:53:39 +0330

    Add product serializer

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
-from apps.products.models import Category
+from apps.products.models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field="name",
+    )
+
+    class Meta:
+        model = Product
+        fields = ["gender", "category", "name", "size", "price", "color"]
+
+        category = serializers.CharField(max_length=30)
