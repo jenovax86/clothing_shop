8f86a2cdb0877bf3200b5ed7b516afa508e7e1de
jenovax86 <shruzvn@gmail.com>
2026-09-10 04:15:49 +0330

    refactor views and urls

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
 ]
--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -1,50 +1,43 @@
 import logging
+import os
 
 from rest_framework import status
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
+from rest_framework.decorators import api_view
 from apps.authentication.services import TokenService, AuthenticationService
 from .serializers import RegisterSerializer, LoginSerializer
 from apps.users.services import UserService
+from ..users.models import Address, User
 
 logger = logging.getLogger(__name__)
 
 
-class RegisterView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        logger.info(f"Post request: {request.data}")
-        serializer = RegisterSerializer(data=request.data)
-
-        if serializer.is_valid():
-            user = UserService.create_user(serializer.validated_data['username'], serializer.validated_data['password'])
-            logger.info(f"User {user.username} created")
-            return Response({
-                "success": True,
-                "message": "User created successfully",
-            }, status=status.HTTP_201_CREATED)
-
+@api_view(['POST'])
+def register_view(request):
+    logger.info(f"Register request")
+    serializer = RegisterSerializer(data=request.data)
+    user_service = UserService(user_model=User, address_model=Address, logger=logger)
+    if not serializer.is_valid():
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    user_service.create_user(serializer.validated_data['username'], serializer.validated_data['password'])
+    return Response({
+        "success": True,
+        "message": "User created successfully",
+    }, status=status.HTTP_201_CREATED)
 
-class LoginView(APIView):
-    permission_classes = [AllowAny]
 
-    def post(self, request):
-        logger.info(f"Post request: {request.data}")
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            logger.info("Serializer validated")
+@api_view(["POST"])
+def login_view(request):
+    logger.info(f"Login request")
+    serializer = LoginSerializer(data=request.data)
+    if not serializer.is_valid(): return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, )
+    auth_service = AuthenticationService(user_model=User, logger=logger, )
+    token_service = TokenService(jwt_secret=os.getenv("JWT_TOKEN_SECRET"), logger=logger, )
 
-            user = AuthenticationService.authenticate_user(serializer.validated_data['username'],
-                                                           serializer.validated_data['password'])
-            logger.info(f"User {user.username} authenticated")
-            access_token = TokenService.generate_access_token(user.id)
-            return Response({
-                "success": True,
-                "token": access_token,
-            }, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    user = auth_service.authenticate_user(serializer.validated_data["username"],
+                                          serializer.validated_data["password"], )
+    access_token = token_service.generate_access_token(user.id)
+    logger.info(f"User {user.username} authenticated.")
+    return Response({"success": True, "message": "User authenticated successfully", "token": access_token, },
+                    status=status.HTTP_200_OK, )
